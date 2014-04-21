fb4892feca95e24e87e5f0b2418290dcb81bd5f8
Bora Akgun <boraakgn@gmail.com>
2014-04-21 21:53:17 +0300

    added session aborted exception

---
+++ Labo.Common.Data/Session/Exceptions/SessionAbortedException.cs
@@ -0,0 +1,46 @@
+﻿namespace Labo.Common.Data.Session.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    using Labo.Common.Exceptions;
+
+    [Serializable]
+    public class SessionAbortedException : CoreLevelException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SessionAbortedException"/> class.
+        /// </summary>
+        public SessionAbortedException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SessionAbortedException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public SessionAbortedException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SessionAbortedException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner.</param>
+        public SessionAbortedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SessionAbortedException"/> class.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination.</param>
+        protected SessionAbortedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
