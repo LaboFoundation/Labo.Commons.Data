19dde6aaf4bcbbf8e5a3d4aecab3096aab11a23b
Bora Akgun <boraakgn@gmail.com>
2014-06-02 03:27:28 +0300

    .

---
+++ Labo.Common.Data.Tests/Service/CustomerService.cs
@@ -0,0 +1,35 @@
+﻿namespace Labo.Common.Data.Tests.Service
+{
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+
+    using Labo.Common.Data.EntityFramework.Session;
+    using Labo.Common.Data.Session;
+    using Labo.Common.Data.Tests.EntityFramework.Contexts.CodeFirst;
+    using Labo.Common.Data.Tests.EntityFramework.Contexts.CodeFirst.Domain;
+
+    public sealed class CustomerService
+    {
+        private readonly ISessionScopeProvider m_SessionScopeProvider;
+
+        public CustomerService()
+        {
+            EntityFrameworkSessionFactoryProvider entityFrameworkSessionFactoryProvider = new EntityFrameworkSessionFactoryProvider();
+            entityFrameworkSessionFactoryProvider.ObjectContextManager.RegisterObjectContextCreator(() => ((IObjectContextAdapter)new CodeFirstDbContext()).ObjectContext);
+            m_SessionScopeProvider = new SessionScopeProvider(entityFrameworkSessionFactoryProvider);
+        }
+
+        public CustomerService(ISessionScopeProvider sessionScopeProvider)
+        {
+            m_SessionScopeProvider = sessionScopeProvider;
+        }
+
+        public Customer GetCustomerByUsername(string username)
+        {
+            using (ISessionScope sessionScope = m_SessionScopeProvider.CreateSessionScope())
+            {
+                return sessionScope.GetRepository<Customer>().Query().SingleOrDefault(x => x.Name == username);
+            }
+        }
+    }
+}
