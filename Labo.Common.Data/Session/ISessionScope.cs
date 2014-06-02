ac4553f018d9fbb29c8e4a06046d4f5f938f81ff
Bora Akgun <boraakgn@gmail.com>
2014-06-02 03:17:15 +0300

    changed method name

--- Labo.Common.Data/Session/ISessionScope.cs
+++ Labo.Common.Data/Session/ISessionScope.cs
@@ -1,4 +1,32 @@
-﻿namespace Labo.Common.Data.Session
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ISessionScope.cs" company="Labo">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2014 Bora Akgun
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy of
+//   this software and associated documentation files (the "Software"), to deal in
+//   the Software without restriction, including without limitation the rights to
+//   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//   the Software, and to permit persons to whom the Software is furnished to do so,
+//   subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//   FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary>
+//   The SessionScope interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Labo.Common.Data.Session
 {
     using System;
 
@@ -10,11 +38,11 @@
     public interface ISessionScope : IDisposable
     {
         /// <summary>
-        /// Creates the repository for <see cref="TEntity"/>.
+        /// Gets the repository for <see cref="TEntity"/>.
         /// </summary>
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <returns>The repository</returns>
-        IRepository<TEntity> CreateRepository<TEntity>() where TEntity : class;
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
 
         /// <summary>
         /// Completes the session scope.
