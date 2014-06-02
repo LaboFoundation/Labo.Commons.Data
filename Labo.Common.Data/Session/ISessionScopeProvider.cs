1fb452cf5f7ffcbb54ae805da28c88fbc0bc10cf
Bora Akgun <boraakgn@gmail.com>
2014-06-02 03:24:44 +0300

    added session interfaces

---
+++ Labo.Common.Data/Session/ISessionScopeProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Labo.Common.Data.Session
+{
+    public interface ISessionScopeProvider
+    {
+        ISessionScope CreateSessionScope(SessionScopeOption sessionScopeOption = SessionScopeOption.Required);
+    }
+}
