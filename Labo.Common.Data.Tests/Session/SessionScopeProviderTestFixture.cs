e65d1585398e0a070848686b39c85455e0830561
Bora Akgun <boraakgn@gmail.com>
2014-06-02 03:27:20 +0300

    added sessionscopeprovider tests

---
+++ Labo.Common.Data.Tests/Session/SessionScopeProviderTestFixture.cs
@@ -0,0 +1,34 @@
+﻿namespace Labo.Common.Data.Tests.Session
+{
+    using Labo.Common.Data.Session;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SessionScopeProviderTestFixture
+    {
+        [Test]
+        public void CreateSessionScopeMustCallCreateSessionFactoryMethodOfSessionFactoryProviderForEveryCall()
+        {
+            ISessionFactoryProvider sessionFactoryProvider = Substitute.For<ISessionFactoryProvider>();
+            ISessionFactory sessionFactory = Substitute.For<ISessionFactory>();
+            sessionFactory.CreateSession().Returns(x => Substitute.For<ISession>());
+            sessionFactoryProvider.CreateSessionFactory().Returns(x => sessionFactory);
+
+            SessionScopeProvider sessionScopeProvider = new SessionScopeProvider(sessionFactoryProvider);
+            using (sessionScopeProvider.CreateSessionScope())
+            {  
+            }
+
+            sessionFactoryProvider.Received(1).CreateSessionFactory();
+
+            using (sessionScopeProvider.CreateSessionScope())
+            {
+            }
+
+            sessionFactoryProvider.Received(2).CreateSessionFactory();
+        }
+    }
+}
