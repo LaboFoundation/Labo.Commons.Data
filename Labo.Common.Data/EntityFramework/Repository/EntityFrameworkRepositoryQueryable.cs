54308b831217c561d3d4ca194342058e8ac6f6e8
Bora Akgun <boraakgn@gmail.com>
2014-06-02 03:23:16 +0300

    implemented entity framework repository classes

---
+++ Labo.Common.Data/EntityFramework/Repository/EntityFrameworkRepositoryQueryable.cs
@@ -0,0 +1,86 @@
+﻿namespace Labo.Common.Data.EntityFramework.Repository
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    using Labo.Common.Data.Repository;
+
+    internal sealed class EntityFrameworkRepositoryQueryable<TEntity> : IRepositoryQueryable<TEntity>
+        where TEntity : class
+    {
+        private IQueryable<TEntity> m_Queryable;
+
+        public EntityFrameworkRepositoryQueryable(IQueryable<TEntity> queryable)
+        {
+            m_Queryable = queryable;
+        }
+
+        public IRepositoryQueryable<TEntity> Include(Expression<Func<TEntity, object>> expression)
+        {
+            m_Queryable = m_Queryable.Include(expression);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return m_Queryable.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)m_Queryable).GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets the type of the element(s) that are returned when the expression tree associated with this instance of <see cref="T:System.Linq.IQueryable"/> is executed.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Type"/> that represents the type of the element(s) that are returned when the expression tree associated with this object is executed.
+        /// </returns>
+        public Type ElementType
+        {
+            get { return m_Queryable.ElementType; }
+        }
+
+        /// <summary>
+        /// Gets the expression tree that is associated with the instance of <see cref="T:System.Linq.IQueryable"/>.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="T:System.Linq.Expressions.Expression"/> that is associated with this instance of <see cref="T:System.Linq.IQueryable"/>.
+        /// </returns>
+        public Expression Expression
+        {
+            get { return m_Queryable.Expression; }
+        }
+
+        /// <summary>
+        /// Gets the query provider that is associated with this data source.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="T:System.Linq.IQueryProvider"/> that is associated with this data source.
+        /// </returns>
+        public IQueryProvider Provider
+        {
+            get { return m_Queryable.Provider; }
+        }
+    }
+}
