6093ae440364ed04988e8d118a2eb8976504f9b4
Bora Akgun <boraakgn@gmail.com>
2014-06-16 12:58:21 +0300

    added paged result test fixture

---
+++ Labo.Common.Data.Tests/Entity/PagedResultTestFixture.cs
@@ -0,0 +1,26 @@
+﻿namespace Labo.Common.Data.Tests.Entity
+{
+    using System.Collections.Generic;
+
+    using Labo.Common.Data.Entity;
+    using Labo.Common.Data.Tests.EntityFramework.Contexts.CodeFirst.Domain;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PagedResultTestFixture
+    {
+        [Test, Sequential]
+        public void TotalPages(
+            [Values(100, 15, 5, 0)]
+            int totalItemsCount,
+            [Values(20, 10, 10, 10)]
+            int pageSize,
+            [Values(5, 2, 1, 0)]
+            int expected)
+        {
+            IPagedResult<Customer> pagedResult = new PagedResult<Customer>(new List<Customer>(), totalItemsCount, 1, pageSize);
+            Assert.AreEqual(expected, pagedResult.TotalPages);
+        }
+    }
+}
